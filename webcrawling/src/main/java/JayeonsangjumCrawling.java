c6dbdf6a0b269d7f9ddb70b527a1c3fd242786d8
sarah2234 <2234sarah@gmail.com>
2022-05-15 20:14:43 +0900

    feat: get categories from 자연드림

--- webcrawling/src/main/java/JayeonsangjumCrawling.java
+++ webcrawling/src/main/java/JayeonsangjumCrawling.java
@@ -12,30 +12,41 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class WebCrawling {
+public class JayeonsangjumCrawling {
     private WebDriver driver;
     private JavascriptExecutor javascriptExecutor;
     private Actions actions;
     private WebElement element;
-    public static String WEB_DRIVER_ID = "webdriver.chrome.driver";
-    public static String WEB_DRIVER_PATH = "chromedriver.exe";
+    private static String WEB_DRIVER_ID = "webdriver.chrome.driver";
+    private static String WEB_DRIVER_PATH = "chromedriver.exe";
 
-    public String url = "http://onlyeco.co.kr/category/%EB%A6%AC%EB%B9%99/50/"; // 자연상점-리빙 카테고리 페이지
-    public ArrayList<String> productLines = new ArrayList<>();
+    private String kitchenPageUrl = "http://onlyeco.co.kr/category/%EB%A6%AC%EB%B9%99/51/"; // 리빙-주방용품 카테고리 페이지
+    private String laundryPageUrl = "http://onlyeco.co.kr/category/%EC%84%B8%ED%83%81%EC%9A%A9%ED%92%88/75/"; // 리빙-세탁용품 카테고리 페이지
+    private String bathPageUrl = "http://onlyeco.co.kr/category/%EC%9A%95%EC%8B%A4%EC%9A%A9%ED%92%88/95/"; // 리빙-욕실용품 카테고리 페이지
+    private ArrayList<String> productNames = new ArrayList<>();
+    private String brandName = "자연상점";
 
-    public WebCrawling() {
+    public JayeonsangjumCrawling() {
         System.setProperty(WEB_DRIVER_ID, WEB_DRIVER_PATH);
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--start-maximized"); // 창 최대
-//        options.addArguments("headless"); // 창 숨김
+        options.addArguments("headless"); // 창 숨김
         options.addArguments("--disable-popup-blocking"); // 팝업창 막기
         this.driver = new ChromeDriver(options);
         this.javascriptExecutor = (JavascriptExecutor)this.driver;
         this.actions = new Actions(this.driver);
+    }
+
+    public void ProductWebCrawling(String category) {
+        String url = "";
+        switch (category) {
+            case "kitchen" -> url = kitchenPageUrl;
+            case "laundry" -> url = laundryPageUrl;
+            case "bath" -> url = bathPageUrl;
+        }
+
         this.driver.get(url);
         WebDriverWait webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(2)); // wait for 2 secs
-//        this.driver.findElement(By.xpath("//*[@id=\"slideCateList\"]/ul/li[4]/a")).click(); // 리빙 카테고리 페이지로 이동
-//        webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(2)); // wait for 2 secs
 
         List<WebElement> descriptionElements = this.driver.findElements(By.className("description")); // 판매 품목을 저장하는 list
         boolean productsLessThan18 = false; // 현재 페이지의 상품 개수가 18개 이하인지 체크
@@ -57,20 +68,22 @@ public class WebCrawling {
             if(productsLessThan18) break; // 상품 개수가 18개 미만이면 해당 페이지가 마지막 페이지임
 
             for (WebElement element : descriptionElements) {
-                productLines.add(element.findElement((By.className("name"))).getText());
+                productNames.add(element.findElement((By.className("name"))).getText());
                 //System.out.println(element.findElement((By.className("name"))).getText());
             }
             descriptionElements.clear();
 
             try {
-                //System.out.println(url + "?page=" + pageCnt);
                 this.driver.get(url + "?page=" + pageCnt); // 다음 페이지로 이동
                 pageCnt++;
             } catch (Exception e)
             {
                 break;
             }
-
         }
     }
+
+    public String getBrandName() { return brandName; }
+
+    public ArrayList<String> getProductNames() { return productNames; }
 }
--- webcrawling/src/main/java/Main.java
+++ webcrawling/src/main/java/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        WebCrawling webCrawling = new WebCrawling();
+        JayeonsangjumCrawling webCrawling = new JayeonsangjumCrawling();
+        webCrawling.ProductWebCrawling("kitchen");
     }
 }
