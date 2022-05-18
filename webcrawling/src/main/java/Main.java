243f375e349520ffea66240b89f7602585e69885
sarah2234 <2234sarah@gmail.com>
2022-05-18 15:15:00 +0900

    feat: web crawling from 닥터노아

--- webcrawling/src/main/java/Main.java
+++ webcrawling/src/main/java/Main.java
@@ -1,7 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-        JayeonsangjumCrawling webCrawling = new JayeonsangjumCrawling();
-        webCrawling.productWebCrawling("kitchen");
+//        JayeonsangjumCrawling webCrawling = new JayeonsangjumCrawling();
+//        webCrawling.productWebCrawling("kitchen");
+
+        DrnoaCrawling drnoaCrawling = new DrnoaCrawling();
+        drnoaCrawling.productWebCrawling_OnlyBath();
     }
 }
